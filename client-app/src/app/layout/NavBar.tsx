5e491d12ca5e7a654acd9b587c90e8a8f364975b
Bob <bob.ntinas@hotmail.com>
2022-12-03 22:17:47 +0200

    End of section 13

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,21 +1,33 @@
+import { observer } from 'mobx-react-lite';
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { Button, Container, Menu } from 'semantic-ui-react';
+import { Link, NavLink } from 'react-router-dom';
+import { Button, Container, Menu, Image, Dropdown, DropdownItem, DropdownMenu } from 'semantic-ui-react';
+import { useStore } from '../stores/store';
 
-export default function NavBar() {    
+export default observer(function NavBar() {
+    const { userStore: { user, logout } } = useStore();
     return (
         <Menu inverted fixed='top'>
             <Container>
                 <Menu.Item as={NavLink} to='/' exact header>
-                    <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}} />
-                    Reactivities    
+                    <img src="/assets/logo.png" alt="logo" style={{ marginRight: '10px' }} />
+                    Reactivities
                 </Menu.Item>
                 <Menu.Item as={NavLink} to='/activities' name='Activities' />
                 <Menu.Item as={NavLink} to='/errors' name='Errors' />
                 <Menu.Item>
                     <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
                 </Menu.Item>
+                <Menu.Item position='right'>
+                    <Image src={user?.image || '/assets/user.png'} avatar spaced='right' />
+                    <Dropdown pointing='top left' text={user?.displayName}>
+                        <DropdownMenu>
+                            <DropdownItem as={Link} to={`/profile/${user?.username}`} text='My Profile' icon='user' />
+                            <DropdownItem onClick={logout} text='logout' icon='power' />
+                        </DropdownMenu>
+                    </Dropdown>
+                </Menu.Item>
             </Container>
         </Menu>
     )
-}
+})
