20744ad46ee42b6b8d36f7d515248c67b6469e59
Bob <bob.ntinas@hotmail.com>
2022-12-05 21:50:16 +0200

    End of section 14

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,9 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Application.Activities;
 using Application.Core;
+using Application.Interfaces;
+using Infrastructure.Security;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
@@ -32,6 +30,7 @@ namespace API.Extensions
              });
              services.AddMediatR(typeof(List.Handler).Assembly);
              services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+             services.AddScoped<IUserAccessor, UserAccessor>();
 
              return services;
         }
